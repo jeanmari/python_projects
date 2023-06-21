deb244e3d9d3f0a5c91c211d11fadefc1bc430a0
Jean Mari <jeanclaveria9@gmail.com>
2023-06-21 12:11:06 +0800

    Completed sa analyzer

--- sa_analyzer.py
+++ sa_analyzer.py
@@ -20,9 +20,9 @@ months_dict = {
     11: 'November',
     12: 'December',
 }
-gathered_months_dict = {}
+
 threshold_per_month = 3
-events_over_threshold = {}
+objects_over_threshold = {}
 
 
 df['date'] = pd.to_datetime(df['date'])
@@ -32,39 +32,50 @@ unique_months = months.unique()
 unique_months = unique_months.tolist()
 
 def init_months():
+    gathered_months_dict = {}
     for month in unique_months:
         gathered_months_dict[months_dict[month]] = 0
     return gathered_months_dict
 
-
-#get unique critical objects
-critical_objects = df["file_name"].unique()
-critical_objects = critical_objects.tolist()
-critical_objects = ["AugueAliquamErat.mov", "IdTurpisInteger.pdf"]
-
-
-#loop thru critical object and match it with file name in excel if it matches, then iterate the number of the specified month
-for idx, critical_object in enumerate(critical_objects):
-    events_over_threshold[critical_object] = init_months()
+def loop_thru_sa(critical_object):
+    iterated_months = init_months()
     for index, row in df.iterrows():
         #if crit object is the same as file name, then iterate the month
         
         if critical_object == row["file_name"]:
             #get the month of that critical object
             month = row["date"].month
-            #iterate the month in the dictionary
-            events_over_threshold[critical_object][months_dict[month]] += 1
-            
-    # if idx == len(critical_objects):
-    #     # print(critical_object) 
-    #     break
+            iterated_months[months_dict[month]] +=1
+    return iterated_months
+
+
+#get unique critical objects
+critical_objects = df["file_name"].unique()
+critical_objects = critical_objects.tolist()
+# critical_objects = ["AugueAliquamErat.mov", "IdTurpisInteger.pdf"]
+
+
+
+#loop thru critical object and match it with file name in excel if it matches, then iterate the number of the specified month
+for idx, critical_object in enumerate(critical_objects):
+    objects_over_threshold[critical_object] = loop_thru_sa(critical_object)
+
+#created new copied variable of objects since we are going to remove objects that has less logs
+copied_objects_over_threshold = objects_over_threshold.copy()
+   
+for object, months in copied_objects_over_threshold.items():
+    #if there is a month that has less than 3 logs for an object, then remove it
+    for key1, month in months.items():
+        if month < threshold_per_month:
+            del objects_over_threshold[object]
+            break
     
 
-# print(events_over_threshold["AugueAliquamErat.mov"])    
-# for key, value in events_over_threshold.items():
-#     print(f"critical_objects: {key}, \nValue: {value}\n\n")
-events_over_threshold["IdTurpisInteger.pdf"] = {"January": 34, "February": 55, "March": 87}
-print(events_over_threshold)
+            
+for key, value in objects_over_threshold.items():
+    print(f"critical_object: {key}, \nLogs_per_month: {value}\n\n")
+            
+    
+print(f"Kindly review this {len(objects_over_threshold)} critical_objects if they are really critical")
 
-# print(len(events_over_threshold))
 
